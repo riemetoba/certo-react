bc840ec3d72c92bd94332dcf728c6ed914143bd5
Toba <riemehasan06@gmail.com>
2026-03-31 23:58:30 +0600

    responsive header

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -4,43 +4,48 @@ import Container from "../Container";
 import Flex from "../Flex";
 import Image from "../Image";
 import logo from "/src/assets/logo.png";
-import { FaAngleDown } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { useState } from "react";
 
 const Header = () => {
-  const[isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <header className="py-4 bg-[#E7EFFA]">
+    <header className="py-4 bg-[#E7EFFA] relative px-2 md:px-0">
       <Container>
-        <Flex>
-          <div className="w-[50%]">
-            <Link to={"/"}>
-              <Image src={logo} />
-            </Link>
-          </div>
-          <div className="w-[40%]">
-            <ul className="flex gap-x-10">
-              <li className="text-[19px] text-common font-Inter font-extrabold cursor-pointer">
-                iPhone
-              </li>
-              <li className="text-[19px] text-common font-Inter font-extrabold cursor-pointer">
-                Android
-              </li>
-              <li className="text-[19px] text-common font-Inter font-extrabold cursor-pointer">
-                Help
-              </li>
-              <li className="text-[19px] text-common font-Inter font-extrabold flex items-center gap-3 cursor-pointer">
-                Company
-                <span>
-                  <FaAngleDown />
-                </span>
-              </li>
-            </ul>
-          </div>
-          <div className="">
-            <Button className={'py-2 px-4 hover:border-[#4335DE] hover:text-white hover:bg-[#4335DE] cursor-pointer'}>Sign in</Button>
+        <Flex className="justify-between items-center">
+          {/* Logo */}
+          <Link to="/" className="w-[120px] md:w-auto">
+            <Image src={logo} />
+          </Link>
+
+          {/* Desktop Menu_hidden on mobile */}
+          <ul className="hidden md:flex gap-x-10 font-extrabold text-common">
+            <li className="cursor-pointer">iPhone</li>
+            <li className="cursor-pointer">Android</li>
+            <li className="cursor-pointer">Help</li>
+            <li className="cursor-pointer">Company</li>
+          </ul>
+
+          {/* Mobile icon */}
+          <div className="flex items-center gap-4">
+            <Button className="hidden md:block py-2 px-4 hover:bg-[#4335DE] hover:text-white">Sign in</Button>
+            <GiHamburgerMenu className="md:hidden text-2xl cursor-pointer" onClick={() => setIsOpen(!isOpen)} />
           </div>
         </Flex>
+
+        {/* Mobile menubar */}
+        {isOpen && (
+          <div className="absolute top-full left-0 w-full bg-white p-5 shadow-md md:hidden text-center z-50">
+            <ul className="flex flex-col gap-y-4 font-bold text-common">
+              <li>iPhone</li>
+              <li>Android</li>
+              <li>Help</li>
+              <li>Company</li>
+              <li><Button className="w-full py-2 bg-[#4335DE] text-white">Sign in</Button></li>
+            </ul>
+          </div>
+        )}
       </Container>
     </header>
   );
