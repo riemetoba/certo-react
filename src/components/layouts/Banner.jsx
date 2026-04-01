84d55f97617c2391c527f8f93ff75abd6810145d
Toba <riemehasan06@gmail.com>
2026-04-02 01:27:58 +0600

    banner er kicchu kaj korlam

--- src/components/layouts/Banner.jsx
+++ src/components/layouts/Banner.jsx
@@ -9,17 +9,17 @@ const Banner = () => {
   return (
     <div className="bg-[url('/src/assets/bannerBg.jpg')] py-18 lg:py-[200px] bg-cover bg-no-repeat bg-center">
       <Container>
-          <div className="px-2">
+          <div className="px-2 md:px-0">
             <h1 className="text-[26px] md:text-6xl font-Inter font-extrabold leading-[50px] md:leading-[75px]">
               Your mobile privacy <br /> is our mission
             </h1>
-            <p className="text-base md:text-[20px] font-Inter w-full md:w-[560px] leading-[30px] my-20">
+            <p className="text-base md:text-[20px] font-Inter w-full md:w-[560px] leading-6 md:leading-[30px] my-12 md:my-18">
               Think your phone has been hacked? Our trusted apps make it easy
               for you to scan, detect and remove threats from your iPhone and
               Android devices.
             </p>
           </div>
-      <Flex className={'md:gap-x-6 flex-col gap-y-5 md:gap-y-0'}>
+      <Flex className={'md:gap-x-6 flex-col gap-y-7 md:gap-y-0'}>
           <Button className="py-3 px-5 border-2 hover:bg-[#FFC247] hover:border-[#FFC247] duration-300 font-Inter font-extrabold rounded-[42px] cursor-pointer bg-transparent flex items-center group">Get Certo for iPhone<FaArrowRightLong className="group-hover:ml-3 opacity-0 group-hover:opacity-100 duration-300"/></Button>
           <Button className="py-3 px-5 border-2 hover:bg-[#FFC247] hover:border-[#FFC247] duration-300 font-Inter font-extrabold rounded-[42px] cursor-pointer bg-transparent flex items-center group">Get Certo for Android<FaArrowRightLong className="group-hover:ml-3 opacity-0 group-hover:opacity-100 duration-300"/></Button>
       
