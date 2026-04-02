3150c4269500c7b509c85ebf40956739eedac67f
Toba <riemehasan06@gmail.com>
2026-04-02 22:34:31 +0600

    getCerto top flex part responsive

--- src/components/layouts/Banner.jsx
+++ src/components/layouts/Banner.jsx
@@ -1,29 +1,32 @@
-
 import { FaArrowRightLong } from "react-icons/fa6";
 import Container from "../Container";
 import Flex from "../Flex";
 import Button from "../Button";
 
-
 const Banner = () => {
   return (
     <div className="bg-[url('/src/assets/bannerBg.jpg')] py-18 lg:py-[200px] bg-cover bg-no-repeat bg-center">
       <Container>
-          <div className="px-2 md:px-0">
-            <h1 className="text-[26px] md:text-6xl font-Inter font-extrabold leading-[50px] md:leading-[75px]">
-              Your mobile privacy <br /> is our mission
-            </h1>
-            <p className="text-base md:text-[20px] font-Inter w-full md:w-[560px] leading-6 md:leading-[30px] my-12 md:my-18">
-              Think your phone has been hacked? Our trusted apps make it easy
-              for you to scan, detect and remove threats from your iPhone and
-              Android devices.
-            </p>
-          </div>
-      <Flex className={'md:gap-x-6 flex-col gap-y-7 md:gap-y-0'}>
-          <Button className="py-3 px-5 border-2 hover:bg-[#FFC247] hover:border-[#FFC247] duration-300 font-Inter font-extrabold rounded-[42px] cursor-pointer bg-transparent flex items-center group">Get Certo for iPhone<FaArrowRightLong className="group-hover:ml-3 opacity-0 group-hover:opacity-100 duration-300"/></Button>
-          <Button className="py-3 px-5 border-2 hover:bg-[#FFC247] hover:border-[#FFC247] duration-300 font-Inter font-extrabold rounded-[42px] cursor-pointer bg-transparent flex items-center group">Get Certo for Android<FaArrowRightLong className="group-hover:ml-3 opacity-0 group-hover:opacity-100 duration-300"/></Button>
-      
-      </Flex>
+        <div className="px-2 md:px-0">
+          <h1 className="text-[26px] md:text-6xl font-Inter font-extrabold leading-[50px] md:leading-[75px]">
+            Your mobile privacy <br /> is our mission
+          </h1>
+          <p className="text-base md:text-[20px] font-Inter w-full md:w-[560px] leading-6 md:leading-[30px] my-12 md:my-18">
+            Think your phone has been hacked? Our trusted apps make it easy for
+            you to scan, detect and remove threats from your iPhone and Android
+            devices.
+          </p>
+        </div>
+        <Flex className={"md:gap-x-6 md:flex-row flex-col gap-y-7 md:gap-y-0"}>
+          <Button className="py-3 px-5 border-2 hover:bg-[#FFC247] hover:border-[#FFC247] duration-300 font-Inter font-extrabold rounded-[42px] cursor-pointer bg-transparent flex items-center group">
+            Get Certo for iPhone
+            <FaArrowRightLong className="group-hover:ml-3 opacity-0 group-hover:opacity-100 duration-300" />
+          </Button>
+          <Button className="py-3 px-5 border-2 hover:bg-[#FFC247] hover:border-[#FFC247] duration-300 font-Inter font-extrabold rounded-[42px] cursor-pointer bg-transparent flex items-center group">
+            Get Certo for Android
+            <FaArrowRightLong className="group-hover:ml-3 opacity-0 group-hover:opacity-100 duration-300" />
+          </Button>
+        </Flex>
       </Container>
     </div>
   );
--- src/components/layouts/GetCerto.jsx
+++ src/components/layouts/GetCerto.jsx
@@ -11,83 +11,57 @@ import Button from "../Button";
 
 const GetCerto = () => {
   return (
-    <div className="bg-[#E7EFFA] py-12 text-center">
-      <h2 className="font-Inter font-extrabold text-4xl w-[738px] m-auto mb-10">
+    <div className="bg-[#E7EFFA] py-12 text-center px-2 md:px-0">
+      <h2 className="font-Inter font-extrabold text-2xl md:text-4xl max-w-[738px] mx-auto mb-10">
         Get your freedom back, stop mobile spyware today
       </h2>
-      <div className="bg-[#ffffff] w-[840px] h-[872px] rounded-[48px] mx-auto drop-shadow-xl p-20">
-        <Flex className={"justify-between"}>
-          <div className="">
-            <Image className={"m-auto"} src={getCertoOne} />
+      <div className="bg-[#ffffff] max-w-[840px] w-full min-h-fit rounded-4xl md:rounded-[48px] mx-auto drop-shadow-xl p-8 md:p-20">
+        
+        {/* flex one */}
+        <Flex className={"md:justify-between flex-col md:flex-row gap-y-10 md:gap-y-0"}>
+          <div className="flex flex-col items-center md:items-start text-center md:text-left">
+            <Image className={"m-auto md:m-0"} src={getCertoOne} />
             <h3 className="font-Inter font-bold text-[19px] text-common py-4">
               Spyware detection
             </h3>
-            <p className="font-Inter text-base text-common w-[195px]">
+            <p className="font-Inter text-base text-common w-full max-w-[195px]">
               Our advanced spyware detection engine can identify if a device
               contains spyware or bugging software.
             </p>
           </div>
-          <div className="">
-            <Image className={"m-auto"} src={getCertoTwo} />
+          <div className="flex flex-col items-center md:items-start text-center md:text-left">
+            <Image className={"m-auto md:m-0"} src={getCertoTwo} />
             <h3 className="font-Inter font-bold text-[19px] text-common py-4">
               Keylogger detection
             </h3>
-            <p className="font-Inter text-base text-common w-[195px]">
+            <p className="font-Inter text-base text-common w-full max-w-[195px]">
               Find malicious keyboards installed on your device that could allow
               someone to record things you type (e.g. passwords).
             </p>
           </div>
-          <div className="">
-            <Image className={"m-auto"} src={getCertoThree} />
+          <div className="flex flex-col items-center md:items-start text-center md:text-left">
+            <Image className={"m-auto md:m-0"} src={getCertoThree} />
             <h3 className="font-Inter font-bold text-[19px] text-common py-4">
               Find tracking apps
             </h3>
-            <p className="font-Inter text-base text-common w-[195px]">
+            <p className="font-Inter text-base text-common w-full max-w-[195px]">
               Check which apps can access your location, microphone or camera.
               Get alerted if a known tracking app is installed.
             </p>
           </div>
         </Flex>
-        <Flex className={"justify-between mt-20"}>
-          <div className="">
-            <Image className={"m-auto"} src={getCertoFour} />
-            <h3 className="font-Inter font-bold text-[19px] text-common py-4">
-              OS integrity check
-            </h3>
-            <p className="font-Inter text-base text-common w-[195px]">
-              Analyze your operating system for signs of tampering that could
-              compromise security, such as Jailbreaking.
-            </p>
-          </div>
-          <div className="">
-            <Image className={"m-auto"} src={getCertoFive} />
-            <h3 className="font-Inter font-bold text-[19px] text-common py-4">
-              Threat removal
-            </h3>
-            <p className="font-Inter text-base text-common w-[195px]">
-              Our intelligent scan will either safely remove threats for you or
-              provide easy-to-follow instructions.
-            </p>
-          </div>
-          <div className="">
-            <Image className={"m-auto"} src={getCertoSix} />
-            <h3 className="font-Inter font-bold text-[19px] text-common py-4">
-              Easy to use
-            </h3>
-            <p className="font-Inter text-base text-common w-[195px]">
-              We create easy to use apps that can check your device for
-              vulnerabilities in a matter of minutes.
-            </p>
-          </div>
-        </Flex>
-        <Flex className={"gap-x-6 mt-15 justify-center"}>
-          <Button className={'py-3 px-5 hover:bg-[#FFC247] hover:border-[#FFC247]'}>
+
+        {/* Buttons Row */}
+        <Flex className={"gap-y-4 sm:gap-y-0 sm:gap-x-6 mt-12 md:mt-15 flex-col sm:flex-row md:justify-center items-center"}>
+          <Button className={'w-full sm:w-auto py-3 px-5 hover:bg-[#FFC247] hover:border-[#FFC247] flex items-center justify-center'}>
             Get Certo for iPhone
             <span className="inline-flex align-middle ml-3">
               <FaArrowRightLong />
             </span>
           </Button>
-          <Button className={'py-3 px-5 hover:bg-[#FFC247] hover:border-[#FFC247]'}>Get Certo for Android</Button>
+          <Button className={'w-full sm:w-auto py-3 px-5 hover:bg-[#FFC247] hover:border-[#FFC247]'}>
+            Get Certo for Android
+          </Button>
         </Flex>
       </div>
     </div>
