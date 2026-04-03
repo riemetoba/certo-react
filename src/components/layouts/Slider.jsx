cc3ed7bb64918b0c98385296247881e8dd8d8453
Toba <riemehasan06@gmail.com>
2026-04-03 23:14:04 +0600

    tag write

--- src/components/layouts/Slider.jsx
+++ src/components/layouts/Slider.jsx
@@ -3,7 +3,7 @@
 export default function Slider() {
   return (
     <section className="bg-[#FDB235] py-12">
-         
+         <h2 className="font-Inter text-[36px] ml-33 font-extrabold">Loved by thousands of iPhone <br />and Android users alike</h2>
     </section>
   )
 }
