cf41ccb049d8d711e544943516a24fdb800c2d13
Toba <riemehasan06@gmail.com>
2026-04-03 23:03:59 +0600

    slider layout

---
+++ src/components/layouts/Slider.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Slider() {
+  return (
+    <section className="bg-[#FDB235] py-12">
+         
+    </section>
+  )
+}
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Banner from "../layouts/Banner"
 import GetCerto from "../layouts/GetCerto"
 import Help from "../layouts/Help"
+import Slider from "../layouts/slider"
 
 
 
@@ -8,6 +9,7 @@ const Home = () => {
   return (
     <>
     <Banner/>
+    <Slider/>
     <GetCerto/>
     <Help/>
     </>
