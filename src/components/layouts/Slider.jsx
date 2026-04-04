0d9378273b36695c0def1c9327b8dc4ff7b19aa9
Toba <riemehasan06@gmail.com>
2026-04-04 23:50:24 +0600

    comment

--- src/components/layouts/Slider.jsx
+++ src/components/layouts/Slider.jsx
@@ -4,6 +4,7 @@ export default function Slider() {
   return (
     <section className="bg-[#FDB235] py-12">
          <h2 className="font-Inter text-[36px] ml-33 font-extrabold mb-10">Loved by thousands of iPhone <br />and Android users alike</h2>
+         {/* main flex start */}
          <div className="flex items-center">
             <div className="bg-white p-10 rounded-4xl w-[20%]">
                 <div className="flex items-center gap-x-2">
