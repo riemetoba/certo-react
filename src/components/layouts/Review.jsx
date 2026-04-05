9fe0116ae72f6396bf892bbdf87394bb9fc42ee4
Toba <riemehasan06@gmail.com>
2026-04-05 22:55:24 +0600

    slider 3 add

--- src/components/layouts/Review.jsx
+++ src/components/layouts/Review.jsx
@@ -6,7 +6,19 @@ export default function Review() {
          <h2 className="font-Inter text-[36px] ml-33 font-extrabold mb-10">Loved by thousands of iPhone <br />and Android users alike</h2>
          {/* main flex start */}
          <div className="flex items-center gap-x-7">
-          {/* slider one  */}
+           {/* slider one  */}
+            <div className="bg-white p-7 rounded-4xl w-[20%] h-[306px]">
+                <div className="flex items-center gap-x-2">
+                    <FaStar className="text-[#FFC247] text-2xl"/>
+                    <FaStar className="text-[#FFC247] text-2xl"/>
+                    <FaStar className="text-[#FFC247] text-2xl"/>
+                    <FaStar className="text-[#FFC247] text-2xl"/>
+                    <FaStar className="text-[#FFC247] text-2xl"/>
+                </div>
+                <p className="text-base font-Inter my-8">Does exactly what it says. Clear to read and understand. This is now the second iPhone we’ve used it on and would certainly recommend this app.</p>
+                <p>colinandmandy94</p>
+            </div>
+          {/* slider two  */}
             <div className="bg-white p-7 rounded-4xl w-[20%] h-[306px]">
                 <div className="flex items-center gap-x-2">
                     <FaStar className="text-[#FFC247] text-2xl"/>
@@ -18,7 +30,8 @@ export default function Review() {
                 <p className="text-base font-Inter my-5">Love the ease and efficiency. Awesome app. Very informative and insightful if wanting to know more about your device. The added breach check.is a great bonus. Check any email of they’ve ever been named in a data breach from years ago. Sweet tool. Love it highly recommend.</p>
                 <p>Jennifer Black</p>
             </div>
-          {/* slider two  */}
+         
+          {/* slider three  */}
             <div className="bg-white p-7 rounded-4xl w-[20%] h-[306px]">
                 <div className="flex items-center gap-x-2">
                     <FaStar className="text-[#FFC247] text-2xl"/>
@@ -27,8 +40,8 @@ export default function Review() {
                     <FaStar className="text-[#FFC247] text-2xl"/>
                     <FaStar className="text-[#FFC247] text-2xl"/>
                 </div>
-                <p className="text-base font-Inter my-8">Does exactly what it says. Clear to read and understand. This is now the second iPhone we’ve used it on and would certainly recommend this app.</p>
-                <p>colinandmandy94</p>
+                <p className="text-base font-Inter my-8">This app is good if you need to identify certain vulnerabilities on your iPhone. If you have any issues, their customer service was quite helpful and responsive.</p>
+                <p>Phillip Colligan</p>
             </div>
          </div>
     </section>
