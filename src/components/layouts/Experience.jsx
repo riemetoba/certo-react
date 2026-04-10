b21cb46dae0b68a46e0574af0a0624b11bdaf2d9
Toba <riemehasan06@gmail.com>
2026-04-10 23:14:19 +0600

    update:  flex and image part

--- src/components/layouts/Experience.jsx
+++ src/components/layouts/Experience.jsx
@@ -1,5 +1,7 @@
 import Container from "../Container";
 import Flex from "../Flex";
+import Image from "../Image";
+import experience from "../../assets/experience.png"
 
 
 export default function Experience() {
@@ -7,7 +9,12 @@ export default function Experience() {
     <section className="py-12">
        <Container>
         <Flex>
-            <h3 className="text-4xl font-extrabold text-common font-Inter">At Certo, mobile security is not an afterthought, it’s what we do.</h3>
+           <div className="">
+             <h3 className="text-4xl font-extrabold text-common font-Inter">At Certo, mobile security is not an afterthought, it’s what we do.</h3>
+           </div>
+           <div className="">
+            <Image src={experience}/>
+           </div>
         </Flex>
        </Container>
     </section>
