748bdcf51473a6c1bace1d49a52b052f8816dbcc
Toba <riemehasan06@gmail.com>
2026-04-10 23:23:26 +0600

    update: paragraph part with width and other customize

--- src/components/layouts/Experience.jsx
+++ src/components/layouts/Experience.jsx
@@ -6,13 +6,14 @@ import experience from "../../assets/experience.png"
 
 export default function Experience() {
   return (
-    <section className="py-12">
+    <section className="py-20">
        <Container>
-        <Flex>
-           <div className="">
-             <h3 className="text-4xl font-extrabold text-common font-Inter">At Certo, mobile security is not an afterthought, it’s what we do.</h3>
+        <Flex className={"justify-between items-start gap-x-10"}>
+           <div className="w-[50%]">
+             <h3 className="text-4xl font-extrabold text-common font-Inter leading-[45px]">At Certo, mobile security <br />is not an afterthought, <br />it’s what we do.</h3>
+             <p className="py-10 text-common text-xl font-Inter w-[430px] leading-[30px]">With years of experience in mobile security and spyware detection, our products have helped countless people safeguard their devices and find peace of mind.</p>
            </div>
-           <div className="">
+           <div className="w-[50%]">
             <Image src={experience}/>
            </div>
         </Flex>
